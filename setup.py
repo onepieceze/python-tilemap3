2d4a4afde9450b28082f642a30bade4e731d223b
onepieceze <onepieceze@163.com>
2020-01-15 22:08:35 +0800

    add setup.py file

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup, Extension
 import os
 
-
+script_root = f"{os.path.dirname(os.path.realpath(__file__))}"
+src_root    = script_root + "/tilemap3/src/"
 
 def get_env(string):
     env = os.getenv(string)
@@ -17,8 +18,8 @@ sdptk_env    = get_env("SDPTK")
 
 extensions = [
     Extension("_tilemap3", 
-              ["tilemap3_wrap.c", "tilemap3.c", "tile.c", "error.c", "isinu.c"],
-              include_dirs=[hdf4_env+"/include", hdf_eos2_env+"/include", sdptk_env+"/include"],
+              [src_root+"tilemap3_wrap.c", src_root+"tilemap3.c", src_root+"tile.c", src_root+"error.c", src_root+"isinu.c"],
+              include_dirs=[script_root+'/tilemap3/include', hdf4_env+"/include", hdf_eos2_env+"/include", sdptk_env+"/include"],
               library_dirs=[hdf4_env+"/lib", hdf_eos2_env+"/lib", sdptk_env+"/lib"],
               libraries=["df", "hdfeos", "PGSTK"]
               )
